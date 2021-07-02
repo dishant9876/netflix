da37bec9f5fb44bd1187a00eb9c49cf4b80668cb
Dishant Wadhwani <wadhwaniyash1234@gmail.com>
2021-07-02 21:36:33 +0530

    Add files via upload

---
+++ src/Sdata.jsx
@@ -0,0 +1,39 @@
+const Sdata = [
+    {
+        id: 1,
+        sname: "DARK",
+        imgsrc: "https://wallpapercave.com/wp/wp4056410.jpg",
+        title: "Netflix Original Series",
+        links: "https://www.netflix.com/in/title/80100172?source=35",
+    },
+    {
+        id: 2,
+        sname: "extra-curricular",
+        imgsrc: "https://m.media-amazon.com/images/M/MV5BNGQxMDcyOTEtZWZkNi00NzgwLWEzNjQtZmFhMGY5ZGRlMTdiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg",
+        title: "Netflix Original Series",
+        links: "https://www.netflix.com/in/title/80990668?source=35",
+    },
+    {
+        id: 3,
+        sname: "Stranger Things",
+        imgsrc: "https://wallpapercave.com/wp/wp1917154.jpg",
+        title: "Netflix Original Series",
+        links: "https://www.netflix.com/in/title/80057281?source=35",
+    },
+    {
+        id: 4,
+        sname: "the Vampire Diaries",
+        imgsrc: "https://images.justwatch.com/poster/8589251/s592",
+        title: "Netflix Original Series",
+        links: "https://www.netflix.com/in/title/70143860?source=35",
+    },
+    {
+        id: 5,
+        sname: "My First-2 Love",
+        imgsrc: "https://i.pinimg.com/474x/7b/19/58/7b19584ec5ffba063a121508e78fe7a4.jpg",
+        title: "Netflix Original Series",
+        links: "https://www.netflix.com/in/title/81026700?source=35",
+    },
+];
+
+export default Sdata;
